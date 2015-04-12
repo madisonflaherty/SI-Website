221f170d021f12ec6b3355663a2a15dcb208e111
Madison Flaherty <mef4824@rit.edu>
2015-04-12 14:00:38 -0400

    Very basic website with only a header. All content is easily changable with javascript variables.

---
+++ static/js/script_customize.js
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////
+//  CUSTOMIZABLE
+//  Only change items within this block
+
+var schoolName = "Rochester Institute of Technology";
+var schoolInitials = "RIT";
+var title = "SI @ " + schoolInitials;
+
+//
+//
+//
+/////////////////////////////////////////////////////////////
+
+//do not edit
+document.getElementById('site_title').innerHTML = title;
+//document.getElementById('schoolName').innerHTML = schoolName;
+//document.getElementById('schoolInitials').innerHTML = schoolInitials;
+document.getElementById('navbar_title').innerHTML = title;
